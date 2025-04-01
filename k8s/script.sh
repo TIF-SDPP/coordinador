f93b593a99a011a5d075d2d2ff8b757ba5b32b88
leoduville <71992403+leoduville@users.noreply.github.com>
2025-03-31 22:49:30 -0300

    agrego replica coordinador

--- k8s/script.sh
+++ k8s/script.sh
@@ -3,7 +3,10 @@
 # Script para aplicar el deployment de Coordinador en Kubernetes
 
 # Aplicar el archivo de configuración de Kubernetes
-kubectl apply -f ./deploy-coordinador.yaml
+kubectl apply -f ./headless-coordinador.yaml
+
+# Aplicar el archivo de configuración de Kubernetes
+kubectl apply -f ./statefulset-coordinador.yaml
 
 # Aplicar el archivo de configuración de Kubernetes
 kubectl apply -f ./service-coordinador.yaml
