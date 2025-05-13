adb25d51946c8c60f8a12f8a7840b085091f982f
leoduville <71992403+leoduville@users.noreply.github.com>
2025-05-13 01:16:22 -0300

    actualizo

--- k8s/script.sh
+++ k8s/script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Script para aplicar el deployment de Coordinador en Kubernetes
+kubectl apply -f ./deploy-coordinador.yaml
 
 # Aplicar el archivo de configuración de Kubernetes
 kubectl apply -f ./headless-coordinador.yaml
 
 # Aplicar el archivo de configuración de Kubernetes
-kubectl apply -f ./statefulset-coordinador.yaml
+#kubectl apply -f ./statefulset-coordinador.yaml
 
 # Aplicar el archivo de configuración de Kubernetes
 #kubectl apply -f ./service-coordinador.yaml
